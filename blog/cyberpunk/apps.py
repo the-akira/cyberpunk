ef88dc5027d6929de2a8fa37fddd58541ef97a24
the-akira <gabrielfelippe90@gmail.com>
2019-05-17 18:10:51 -0300

    Primeiro commit, Django Girls App

---
+++ blog/cyberpunk/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CyberpunkConfig(AppConfig):
+    name = 'cyberpunk'
---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/talantyr/Documentos/Projetos/Python Experimentos/projeto_django/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
