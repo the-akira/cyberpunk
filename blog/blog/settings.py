40a5a14974ee2551cda829523615948b4b451e40
the-akira <gabrielfelippe90@gmail.com>
2019-05-18 22:46:41 -0300

    alteracoes configucaroes

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1md1$whs18u+748nw0ts!x!vmwrw!cz@9wd&*8s)%+ou6-q9vq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/cyberpunk/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
